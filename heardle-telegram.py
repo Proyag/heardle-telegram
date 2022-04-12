595117fc93b05618724c88a88a488397bff715e8
Proyag <proyag.pal@gmail.com>
2022-04-12 15:38:06 +0100

    Do Python main properly

--- heardle-telegram.py
+++ heardle-telegram.py
@@ -1,8 +1,9 @@
 from heardle_telegram.ytmusic_library import Library
 from heardle_telegram.process_song import ClipGenerator
 
-# Pick a random song
-song = Library().get_random_song()
+if __name__ == '__main__':
+    # Pick a random song
+    song = Library().get_random_song()
 
-# Download the song and generate clips
-clip_generator = ClipGenerator().prepare_song(song)
+    # Download the song and generate clips
+    clip_generator = ClipGenerator().prepare_song(song)
