6bae9d2c02d0bd030d98f45dd863a9b109bb8b58
Proyag <proyag.pal@gmail.com>
2022-04-12 18:27:52 +0100

    /start command launches song preparation

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 Like [Heardle](https://www.heardle.app), but with a customised list of songs and [for now] in the form of a telegram bot instead of a web UI.
 
 ## Setup
+
+### Install dependencies
 ```bash
 conda create -n heardle-telegram python=3.10
 sudo apt install ffmpeg
 pip install -r requirements.txt
 ```
+
+### Configure `ytmusicapi` headers
 Then set up ytmusicapi headers according to https://ytmusicapi.readthedocs.io/en/latest/setup.html.
 * Go to https://music.youtube.com and ensure you are logged in
 * Find an authenticated POST request. The simplest way is to filter by /browse using the search bar of the developer tools. If you don’t see the request, try scrolling down a bit or clicking on the library button in the top bar.
@@ -22,6 +26,15 @@ Then set up ytmusicapi headers according to https://ytmusicapi.readthedocs.io/en
 * Verify that the request looks like this: **Status** 200, **Type** xhr, **Name** `browse?...`
 * Click on the Name of any matching request. In the “Headers” tab, scroll to the section “Request headers” and copy everything starting from “accept: */*” to the end of the section
 </details>
+
+### Configure Telegram Bot API
+Next, create a file called `telegram_config.json` which should be a JSON file with the following info:
+```json
+{
+    "api_token": "The bot API token",
+    "chat_id": "Your telegram chat ID"
+}
+```
 
 ## Run
 Run a game with
--- heardle-telegram.py
+++ heardle-telegram.py
@@ -1,12 +1,38 @@
 import logging
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
-
+import json
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from heardle_telegram.ytmusic_library import Library
 from heardle_telegram.process_song import ClipGenerator
 
-if __name__ == '__main__':
+def start(update: Update, context: CallbackContext) -> None:
+    """Prepare a song when the command /start is issued."""
+    logging.info("/start command received")
+    update.message.reply_text("Preparing a song... Wait for a READY notification")
     # Pick a random song
     song = Library().get_random_song()
-
     # Download the song and generate clips
     clip_generator = ClipGenerator().prepare_song(song)
+    update.message.reply_text("READY")
+
+
+def main() -> None:
+    # Configure Telegram API
+    telegram_config = json.load(open('telegram_config.json'))
+
+    # https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/echobot.py
+    updater = Updater(telegram_config['api_token'])
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+    # Add command handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    # Start the Bot
+    updater.start_polling()
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
