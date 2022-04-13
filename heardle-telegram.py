78796f2b9a220132b3feb4ecf18cc821a4c5a4a9
Proyag <proyag.pal@gmail.com>
2022-04-13 09:37:53 +0100

    Set up basic bot command structure

--- heardle-telegram.py
+++ heardle-telegram.py
@@ -1,23 +1,37 @@
 import logging
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
 import json
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from heardle_telegram.ytmusic_library import Library
 from heardle_telegram.process_song import ClipGenerator
 
 def start(update: Update, context: CallbackContext) -> None:
-    """Prepare a song when the command /start is issued."""
+    """Start a game when the command /start is issued."""
     logging.info("/start command received")
-    update.message.reply_text("Preparing a song... Wait for a READY notification")
+    
+def help(update: Update, context: CallbackContext) -> None:
+    """Help message"""
+    logging.info("/help command received")
+    update.message.reply_text(
+        "Heardle telegram bot\n"
+        "/start: Start playing current game\n"
+        "/status: Check whether game is running\n"
+        "/help: Get this help message\n"
+    )
+
+def status(update: Update, context: CallbackContext) -> None:
+    """Check whether game is running and current game ID"""
+    logging.info("/status command received")
+    update.message.reply_text("Game running")
+
+
+def main() -> None:
     # Pick a random song
     song = Library().get_random_song()
     # Download the song and generate clips
     clip_generator = ClipGenerator().prepare_song(song)
-    update.message.reply_text("READY")
 
-
-def main() -> None:
     # Configure Telegram API
     telegram_config = json.load(open('telegram_config.json'))
 
@@ -25,14 +39,17 @@ def main() -> None:
     updater = Updater(telegram_config['api_token'])
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-    # Add command handlers
+
+    # Command handlers
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("status", status))
+
     # Start the Bot
     updater.start_polling()
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+    logging.info("New game is up")
 
 if __name__ == '__main__':
     main()
